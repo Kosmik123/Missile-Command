bc5fd11b581b91ebcde36274213d7a4f08952bcf
Kosmik123 <malpa.w.klapkach@gmail.com>
2022-03-25 22:03:54 +0100

    Make GameController a singleton

--- Assets/Scripts/ExplodeAtPosition.cs
+++ Assets/Scripts/ExplodeAtPosition.cs
@@ -4,19 +4,12 @@ namespace MissileCommand
 {
     public class ExplodeAtPosition : MonoBehaviour
     {
-        private float delta = 0.05f;
+        private float delta = 0.1f;
 
         [SerializeField] private Vector3 explodePosition;
+        public Vector3 ExplodePosition { get => explodePosition; set => explodePosition = value; }
 
-        private void Update()
-        {
-            if((transform.position - explodePosition).magnitude < delta)
-            {
-                Explode();
-            }
-        }
-
-        private void Explode()
+        public void Explode()
         {
             ProjectileInstantiator.InstantiateExplosion(transform.position);
             Destroy(gameObject);
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,20 +6,25 @@ namespace MissileCommand {
 
     public class GameController : MonoBehaviour
     {
+        public static GameController Instance { get; private set; }
+
         [Header("Settings")]
         [SerializeField] private new Camera camera;
         [SerializeField] private CrosshairController crosshair;
         [SerializeField] private ShooterController[] cannons;
 
+        [Header("States")]
+        [SerializeField] private float points;
+
         private void Awake()
         {
+            Instance = Singleton.MakeInstance(this, Instance);
             crosshair.rectProvider = camera.GetComponent<CameraRectProvider>();
 
             foreach (var cannon in cannons)
             {
                 cannon.Target = crosshair.transform;
             }
-
         }
 
 
