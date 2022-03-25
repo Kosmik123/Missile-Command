5fa6ceca046f0c76dd98844b6f802fa2637d4979
Kosmik123 <malpa.w.klapkach@gmail.com>
2022-03-25 23:10:21 +0100

    Add some UI elements
    Added score and highscore indicators
    Added UIController

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+
 using UnityEngine;
 
-namespace MissileCommand {
+
+namespace MissileCommand
+{
+
 
     public class GameController : MonoBehaviour
     {
         public static GameController Instance { get; private set; }
+
+        public static event Action<int> OnPointsChanged;
 
         [Header("Settings")]
         [SerializeField] private new Camera camera;
@@ -15,6 +20,7 @@ namespace MissileCommand {
 
         [Header("States")]
         [SerializeField] private float points;
+        public float Points { get => points; private set => points = value; }
 
         private void Awake()
         {
@@ -25,6 +31,15 @@ namespace MissileCommand {
             {
                 cannon.Target = crosshair.transform;
             }
+        }
+
+        public void AddPoints(int points)
+        {
+            if (points < 0)
+                return;
+
+            this.points += points;
+            OnPointsChanged?.Invoke(points);
         }
 
 
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,36 @@
+﻿
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace MissileCommand
+{
+    public class UIController : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text pointsIndicator;
+        [SerializeField] private TMP_Text highscoreIndicator;
+
+        private void OnEnable()
+        {
+            GameController.OnPointsChanged += RefreshPoints; 
+        }
+
+        private void RefreshPoints(int points)
+        {
+            pointsIndicator.text = points.ToString();
+        }
+
+        private void RefreshHighscore(int highscore)
+        {
+            highscoreIndicator.text = highscore.ToString();
+        }
+
+
+
+        private void OnDisable()
+        {
+            GameController.OnPointsChanged -= RefreshPoints; 
+        }
+    }
+}
