cb89e5cdaf57d02e931ec7d4d74427bf134f0451
Kosmik123 <malpa.w.klapkach@gmail.com>
2022-03-25 17:10:57 +0100

    Add explosion
    Created explosion prefab
    Added explosion functionality to ExplosionController

--- Assets/Scripts/ExplosionController.cs
+++ Assets/Scripts/ExplosionController.cs
@@ -2,8 +2,35 @@
 
 namespace MissileCommand
 {
+    [RequireComponent(typeof(CircleCollider2D))]
     public class ExplosionController : MonoBehaviour
     {
+        private new CircleCollider2D collider;
+
+        [Header("Settings")]
+        [SerializeField] private AnimationCurve sizeChangeCurve;
+        [SerializeField] private float maxRadius;
+        [SerializeField] private float speed;
+
+        private float progress = 0;
+        private float radius;
+
+
+        private void Awake()
+        {
+            collider = GetComponent<CircleCollider2D>();
+        }
+
+        private void Update()
+        {
+            progress += speed * Time.deltaTime;
+            radius = maxRadius * sizeChangeCurve.Evaluate(progress);
+
+            collider.radius = radius;
+
+            if (progress > 1)
+                Destroy(gameObject);
+        }
 
     }
 }
